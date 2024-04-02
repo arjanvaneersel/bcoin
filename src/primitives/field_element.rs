42133e5d37f5d7721301f6dfac0239251c69c8f9
Arjan van Eersel <arjanvaneersel@gmail.com>
2024-04-02 16:21:04 +0300

    Implement add, sub, mul, div and pow for FieldElement

--- src/primitives/field_element.rs
+++ src/primitives/field_element.rs
@@ -1,27 +1,46 @@
-use num::One;
+use mod_exp::mod_exp;
+use num::{traits::Euclid, One, Zero};
 
 /// Supertrait that numbers for FieldElement need to implement.
 pub trait Number<T>:
     Default
     + Clone
-    + std::cmp::PartialEq
-    + std::cmp::PartialOrd
-    + std::fmt::Debug
-    + std::ops::Add<Output = T>
-    + std::ops::Sub<Output = T>
+    + Copy
     + One
+    + Zero
+    + PartialEq
+    + PartialOrd
+    + Euclid
+    + num::Bounded
+    + num::Num
+    + std::fmt::Debug
+    + std::ops::Add<T, Output = T>
+    + std::ops::Sub<T, Output = T>
+    + std::ops::Mul<T, Output = T>
+    + std::ops::Div<T, Output = T>
+    + std::ops::Rem<T, Output = T>
+    + std::ops::Shr<T, Output = T>
 {
 }
 
 impl<T> Number<T> for T where
     T: Default
         + Clone
-        + std::cmp::PartialEq
-        + std::cmp::PartialOrd
+        + Copy
+        + One
+        + Zero
+        + PartialEq
+        + PartialOrd
+        + Euclid
+        + num::Bounded
+        + num::Num
         + std::fmt::Debug
         + std::ops::Add<Output = T>
         + std::ops::Sub<Output = T>
-        + One
+        + std::ops::Mul<T, Output = T>
+        + std::ops::Div<T, Output = T>
+        + std::ops::Rem<T, Output = T>
+        + std::ops::Shr<T, Output = T>
 {
 }
 
@@ -49,16 +68,24 @@ where
 
         FieldElement(num, prime)
     }
-}
 
-// Implement the From trait for easy casting from numbers into FieldElement.
-impl<T> From<T> for FieldElement<T>
-where
-    T: Number<T>,
-{
-    fn from(value: T) -> Self {
-        let prime = value.clone() + One::one();
-        FieldElement::new(value, prime)
+    #[allow(dead_code)]
+    // TODO: Remove #[allow(dead_code)] once it's being used.
+    pub fn pow(&self, exp: T) -> FieldElement<T> {
+        // If the exponent is zero, the value 1 should be returned.
+        if exp == T::zero() {
+            return FieldElement(T::one(), self.1);
+        }
+
+        // Ensure a positive exponent.
+        let n = T::from(exp.rem_euclid(&(self.1 - T::one())));
+
+        // Calculate while capping to prime.
+        dbg!(self.0, n, self.1);
+        let num = mod_exp(self.0, n, self.1);
+        dbg!(num);
+
+        FieldElement(num, self.1)
     }
 }
 
@@ -79,22 +106,79 @@ impl<T> std::ops::Add for FieldElement<T>
 where
     T: Number<T>,
 {
-    type Output = FieldElement<T>;
+    type Output = Self;
 
     fn add(self, rhs: Self) -> Self::Output {
-        let highest_prime = match self.1 >= rhs.1 {
-            true => self.1,
-            false => rhs.1,
-        };
+        // Ensure that the primes are equal.
+        if self.1 != rhs.1 {
+            panic!("Sides are of different fields")
+        }
 
-        let num = self.0.clone() + rhs.0.clone();
+        // Add the numbers while capping to the prime value.
+        let num = (self.0 + rhs.0) % self.1.clone();
+        FieldElement::new(num, self.1)
+    }
+}
 
-        let prime = match num > highest_prime {
-            true => num.clone() + One::one(),
-            false => highest_prime,
-        };
+// Implement the sub operator.
+impl<T> std::ops::Sub for FieldElement<T>
+where
+    T: Number<T>,
+{
+    type Output = Self;
 
-        FieldElement::new(num, prime)
+    fn sub(self, rhs: Self) -> Self::Output {
+        // Ensure that the primes are equal.
+        if self.1 != rhs.1 {
+            panic!("Sides are of different fields")
+        }
+
+        let num = (self.0 - rhs.0) % self.1.clone();
+        FieldElement::new(num, self.1)
+    }
+}
+
+// Implement the mul operator.
+impl<T> std::ops::Mul for FieldElement<T>
+where
+    T: Number<T>,
+{
+    type Output = Self;
+
+    fn mul(self, rhs: Self) -> Self::Output {
+        // Ensure that the primes are equal.
+        if self.1 != rhs.1 {
+            panic!("Sides are of different fields")
+        }
+
+        // Multiply the numbers while capping to the prime value.
+        let num = (self.0 * rhs.0) % self.1.clone();
+        FieldElement::new(num, self.1)
+    }
+}
+
+// Implement the mul operator.
+impl<T> std::ops::Div for FieldElement<T>
+where
+    T: Number<T>,
+{
+    type Output = Self;
+
+    fn div(self, rhs: Self) -> Self::Output {
+        // Ensure that the primes are equal.
+        if self.1 != rhs.1 {
+            panic!("Sides are of different fields")
+        }
+
+        let two = T::one() + T::one();
+
+        // use Fermat's little theorem:
+        // self.num**(p-1) % p == 1
+        // this means:
+        // 1/n == pow(n, p-2, p)
+        // we return an element of the same class
+        let num = self.0 * mod_exp(rhs.0, self.1.clone() - two, self.1.clone()) % self.1.clone();
+        FieldElement::new(num, self.1)
     }
 }
 
@@ -113,12 +197,44 @@ mod tests {
 
     #[test]
     fn add_works() {
-        let a = FieldElement::new(10, 25);
-        let b = FieldElement::new(12, 13);
+        let a = FieldElement(7, 13);
+        let b = FieldElement(12, 13);
+        let c = FieldElement(6, 13);
+        assert_eq!(a + b, c);
+    }
 
-        assert!(a != b);
+    #[test]
+    fn sub_works() {
+        let a = FieldElement(35, 13);
+        let b = FieldElement(7, 13);
+        let c = FieldElement(2, 13);
+        assert_eq!(a - b, c);
+    }
 
-        let result = a + b;
-        assert_eq!(result, 22.into());
+    #[test]
+    fn mul_works() {
+        let a = FieldElement(3, 13);
+        let b = FieldElement(12, 13);
+        let c = FieldElement(10, 13);
+        assert_eq!(a * b, c);
+    }
+
+    #[test]
+    fn pow_works() {
+        let a = FieldElement(3, 13);
+        let b = FieldElement(1, 13);
+        assert_eq!(a.pow(3), b);
+
+        let a = FieldElement(7, 13);
+        let b = FieldElement(8, 13);
+        assert_eq!(a.pow(-3), b);
+    }
+
+    #[test]
+    fn div_works() {
+        let a = FieldElement(10, 13);
+        let b = FieldElement(2, 13);
+        let c = FieldElement(5, 13);
+        assert_eq!(a / b, c);
     }
 }
