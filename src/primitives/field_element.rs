7141c399d9a7f33819ec153f91bc7d654f83138e
Arjan van Eersel <arjanvaneersel@gmail.com>
2024-04-02 09:07:13 +0300

    Add doc comments

--- src/primitives/field_element.rs
+++ src/primitives/field_element.rs
@@ -1,18 +1,45 @@
+/// A finite field
+///
+/// The mathematical definition of a finite field is:
+/// 1. If a and b are in the set, then a + b and a * b are in the set. (Closed property)
+/// 2. 0 exists and has the property a + 0 = a. (Additive identity)
+/// 3. 1 exists and has the property a * 1 = a. (Multiplicative identity)
+/// 4. If a is in the set then -a is in the set, which is defined as the value that makes a + (-a) = 0. (Additive inverse)
+/// 5. If a is in the set and is not 0, a^-1 is in the set where a*a^-1 = 1. (Multiplicative inverse)
+///
+/// The mathematical notation is Fp = {0,1,2,...p-1}.
 pub struct FieldElement<T>(T, T);
 
-impl<T> FieldElement<T> 
-where T: Default + std::cmp::PartialEq + std::cmp::PartialOrd + std::fmt::Debug + std::ops::Add + std::ops::Sub {
-    pub fn new(num:T, prime:T) -> FieldElement<T> {
+// Constructor with trait bounds.
+impl<T> FieldElement<T>
+where
+    T: Default
+        + std::cmp::PartialEq
+        + std::cmp::PartialOrd
+        + std::fmt::Debug
+        + std::ops::Add
+        + std::ops::Sub,
+{
+    pub fn new(num: T, prime: T) -> FieldElement<T> {
         if num >= prime || num < T::default() {
-            panic!("Num {:?} not in field range 0 to {:?}", num, prime);
+            panic!("{:?} is not in field range 0 to {:?}", num, prime);
         }
 
         FieldElement(num, prime)
     }
 }
 
-impl<T> From<T> for FieldElement<T> 
-where T: Default + Clone + std::cmp::PartialEq + std::cmp::PartialOrd + std::fmt::Debug + std::ops::Add + std::ops::Sub {
+// Implement the From trait for easy casting from numbers into FieldElement.
+impl<T> From<T> for FieldElement<T>
+where
+    T: Default
+        + Clone
+        + std::cmp::PartialEq
+        + std::cmp::PartialOrd
+        + std::fmt::Debug
+        + std::ops::Add
+        + std::ops::Sub,
+{
     fn from(value: T) -> Self {
         FieldElement::new(value.clone(), value)
     }
@@ -30,8 +57,16 @@ impl<T: std::fmt::Debug> std::fmt::Debug for FieldElement<T> {
     }
 }
 
-impl<T> std::ops::Add for FieldElement<T> 
-where T: Default + std::cmp::PartialEq + std::cmp::PartialOrd + std::fmt::Debug + std::ops::Add<Output = T> + std::ops::Sub<Output = T> {
+// Implement the add operator.
+impl<T> std::ops::Add for FieldElement<T>
+where
+    T: Default
+        + std::cmp::PartialEq
+        + std::cmp::PartialOrd
+        + std::fmt::Debug
+        + std::ops::Add<Output = T>
+        + std::ops::Sub<Output = T>,
+{
     type Output = FieldElement<T>;
 
     fn add(self, rhs: Self) -> Self::Output {
@@ -46,6 +81,7 @@ where T: Default + std::cmp::PartialEq + std::cmp::PartialOrd + std::fmt::Debug 
     }
 }
 
+// Implement the eq operator.
 impl<T: std::cmp::PartialEq> std::cmp::PartialEq for FieldElement<T> {
     fn eq(&self, other: &Self) -> bool {
         self.0 == other.0
