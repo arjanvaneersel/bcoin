6f53bc96dd889104ba146c73ebb9d22b6b2a9830
Arjan van Eersel <arjanvaneersel@gmail.com>
2024-04-03 10:31:53 +0300

    Improve tests to catch edge cases

--- src/primitives/field_element.rs
+++ src/primitives/field_element.rs
@@ -62,16 +62,25 @@ where
     T: Number<T>,
 {
     pub fn new(num: T, prime: T) -> FieldElement<T> {
-        if num >= prime || num < T::default() {
-            panic!("{:?} is not in field range 0 to {:?}", num, prime);
-        }
+        FieldElement(num, prime).__pass_if_valid_range()
+    }
 
-        FieldElement(num, prime)
+    fn __ensure_valid_range(&self) {
+        if self.0 >= self.1 || self.0 < T::default() {
+            panic!("{:?} is not in field range 0 to {:?}", self.0, self.1);
+        }
+    }
+
+    fn __pass_if_valid_range(self) -> FieldElement<T> {
+        self.__ensure_valid_range();
+        self
     }
 
     #[allow(dead_code)]
     // TODO: Remove #[allow(dead_code)] once it's being used.
     pub fn pow(&self, exp: T) -> FieldElement<T> {
+        self.__ensure_valid_range();
+
         // If the exponent is zero, the value 1 should be returned.
         if exp == T::zero() {
             return FieldElement(T::one(), self.1);
@@ -109,6 +118,9 @@ where
     type Output = Self;
 
     fn add(self, rhs: Self) -> Self::Output {
+        self.__ensure_valid_range();
+        rhs.__ensure_valid_range();
+
         // Ensure that the primes are equal.
         if self.1 != rhs.1 {
             panic!("Sides are of different fields")
@@ -128,6 +140,9 @@ where
     type Output = Self;
 
     fn sub(self, rhs: Self) -> Self::Output {
+        self.__ensure_valid_range();
+        rhs.__ensure_valid_range();
+
         // Ensure that the primes are equal.
         if self.1 != rhs.1 {
             panic!("Sides are of different fields")
@@ -146,6 +161,9 @@ where
     type Output = Self;
 
     fn mul(self, rhs: Self) -> Self::Output {
+        self.__ensure_valid_range();
+        rhs.__ensure_valid_range();
+
         // Ensure that the primes are equal.
         if self.1 != rhs.1 {
             panic!("Sides are of different fields")
@@ -165,6 +183,9 @@ where
     type Output = Self;
 
     fn div(self, rhs: Self) -> Self::Output {
+        self.__ensure_valid_range();
+        rhs.__ensure_valid_range();
+
         // Ensure that the primes are equal.
         if self.1 != rhs.1 {
             panic!("Sides are of different fields")
@@ -172,11 +193,10 @@ where
 
         let two = T::one() + T::one();
 
-        // use Fermat's little theorem:
+        // Use Fermat's little theorem:
         // self.num**(p-1) % p == 1
         // this means:
         // 1/n == pow(n, p-2, p)
-        // we return an element of the same class
         let num = self.0 * mod_exp(rhs.0, self.1 - two, self.1) % self.1;
         FieldElement::new(num, self.1)
     }
@@ -196,6 +216,12 @@ mod tests {
     use super::*;
 
     #[test]
+    #[should_panic]
+    fn can_not_create_out_of_range_field_element() {
+        FieldElement::new(13, 7);
+    }
+
+    #[test]
     fn add_works() {
         let a = FieldElement(7, 13);
         let b = FieldElement(12, 13);
@@ -204,11 +230,59 @@ mod tests {
     }
 
     #[test]
+    #[should_panic(expected = "12 is not in field range 0 to 10")]
+    fn add_panics_on_lhs_out_of_range() {
+        let a = FieldElement(12, 10);
+        let b = FieldElement(3, 10);
+        let _ = a + b;
+    }
+
+    #[test]
+    #[should_panic(expected = "12 is not in field range 0 to 10")]
+    fn add_panics_on_rhs_out_of_range() {
+        let a = FieldElement(12, 10);
+        let b = FieldElement(3, 10);
+        let _ = b + a;
+    }
+
+    #[test]
+    #[should_panic(expected = "Sides are of different fields")]
+    fn add_panics_on_different_fields() {
+        let a = FieldElement(7, 13);
+        let b = FieldElement(12, 100);
+        let _ = a + b;
+    }
+
+    #[test]
     fn sub_works() {
-        let a = FieldElement(35, 13);
-        let b = FieldElement(7, 13);
-        let c = FieldElement(2, 13);
+        let a = FieldElement(35, 40);
+        let b = FieldElement(7, 40);
+        let c = FieldElement(28, 40);
         assert_eq!(a - b, c);
+    }
+
+    #[test]
+    #[should_panic(expected = "12 is not in field range 0 to 10")]
+    fn sub_panics_on_lhs_out_of_range() {
+        let a = FieldElement(12, 10);
+        let b = FieldElement(7, 10);
+        let _ = a - b;
+    }
+
+    #[test]
+    #[should_panic(expected = "12 is not in field range 0 to 10")]
+    fn sub_panics_on_rhs_out_of_range() {
+        let a = FieldElement(12, 10);
+        let b = FieldElement(7, 10);
+        let _ = b - a;
+    }
+
+    #[test]
+    #[should_panic(expected = "Sides are of different fields")]
+    fn sub_panics_on_different_fields() {
+        let a = FieldElement(12, 13);
+        let b = FieldElement(7, 100);
+        let _ = a - b;
     }
 
     #[test]
@@ -220,6 +294,30 @@ mod tests {
     }
 
     #[test]
+    #[should_panic(expected = "12 is not in field range 0 to 10")]
+    fn mul_panics_on_lhs_out_of_range() {
+        let a = FieldElement(12, 10);
+        let b = FieldElement(7, 10);
+        let _ = a * b;
+    }
+
+    #[test]
+    #[should_panic(expected = "12 is not in field range 0 to 10")]
+    fn mul_panics_on_rhs_out_of_range() {
+        let a = FieldElement(12, 10);
+        let b = FieldElement(7, 10);
+        let _ = b * a;
+    }
+
+    #[test]
+    #[should_panic(expected = "Sides are of different fields")]
+    fn mul_panics_on_different_fields() {
+        let a = FieldElement(7, 13);
+        let b = FieldElement(12, 100);
+        let _ = a * b;
+    }
+
+    #[test]
     fn pow_works() {
         let a = FieldElement(3, 13);
         let b = FieldElement(1, 13);
@@ -228,6 +326,17 @@ mod tests {
         let a = FieldElement(7, 13);
         let b = FieldElement(8, 13);
         assert_eq!(a.pow(-3), b);
+
+        let a = FieldElement(7, 13);
+        let b = FieldElement(1, 13);
+        assert_eq!(a.pow(0), b);
+    }
+
+    #[test]
+    #[should_panic(expected = "12 is not in field range 0 to 10")]
+    fn pow_panics_on_out_of_range() {
+        let a = FieldElement(12, 10);
+        let _ = a.pow(3);
     }
 
     #[test]
@@ -236,5 +345,36 @@ mod tests {
         let b = FieldElement(2, 13);
         let c = FieldElement(5, 13);
         assert_eq!(a / b, c);
+    }
+
+    #[test]
+    #[should_panic(expected = "12 is not in field range 0 to 10")]
+    fn div_panics_on_lhs_out_of_range() {
+        let a = FieldElement(12, 10);
+        let b = FieldElement(7, 10);
+        let _ = a / b;
+    }
+
+    #[test]
+    #[should_panic(expected = "12 is not in field range 0 to 10")]
+    fn div_panics_on_rhs_out_of_range() {
+        let a = FieldElement(12, 10);
+        let b = FieldElement(7, 10);
+        let _ = b / a;
+    }
+
+    #[test]
+    #[should_panic(expected = "Sides are of different fields")]
+    fn div_panics_on_different_fields() {
+        let a = FieldElement(12, 13);
+        let b = FieldElement(7, 100);
+        let _ = a / b;
+    }
+
+    #[test]
+    fn debug_and_display_impl_works() {
+        let a = FieldElement(10, 13);
+        assert_eq!(format!("{:?}", a), "10".to_string());
+        assert_eq!(format!("{}", a), "10".to_string());
     }
 }
