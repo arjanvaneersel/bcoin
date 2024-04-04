1f62f3275d6f6e595d24a6dce9f96f556d704a39
Arjan van Eersel <arjanvaneersel@gmail.com>
2024-04-04 12:02:08 +0300

    Refactor code for Point so that it returns a result instead of panicing on invalid input

--- src/primitives/elliptic_curve/point.rs
+++ src/primitives/elliptic_curve/point.rs
@@ -1,4 +1,5 @@
 use num::{pow::Pow, One, Zero};
+use std::fmt::Display;
 
 /// Supertrait that numbers for FieldElement need to implement.
 pub trait Number:
@@ -37,12 +38,28 @@ impl<T> Number for T where
 {
 }
 
+#[derive(Clone, Debug, PartialEq)]
+pub enum Error<T> {
+    InvalidPoint,
+    NotOnCurve(T, T),
+}
+
+impl<T: Number> Display for Error<T> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let s = match self {
+            Self::InvalidPoint => "Invalid point".into(),
+            Self::NotOnCurve(x, y) => format!("({:?}, {:?}) is not on the curve", x, y),
+        };
+        write!(f, "{}", s)
+    }
+}
+
+impl<T: Number> std::error::Error for Error<T> {}
+
 #[derive(Debug, Clone, Copy, PartialEq)]
-struct Point<T> {
-    a: T,
-    b: T,
-    x: Option<T>,
-    y: Option<T>,
+pub enum Point<T> {
+    Point { a: T, b: T, x: T, y: T },
+    Infinite { a: T, b: T },
 }
 
 impl<T: Number> Eq for Point<T> {}
@@ -50,21 +67,46 @@ impl<T: Number> Eq for Point<T> {}
 impl<T: Number> Point<T> {
     #[allow(dead_code)]
     // TODO: Remove #[allow(dead_code)] once it's being used.
-    pub fn new(x: Option<T>, y: Option<T>, a: T, b: T) -> Point<T> {
-        Point { a, b, x, y }.__pass_if_valid()
-    }
-
-    fn __ensure_valid(&self) {
-        if let (Some(x), Some(y)) = (self.x, self.y) {
-            if y.pow(2_u8) != x.pow(3_u8) + self.a * x + self.b {
-                panic!("({:?}, {:?}) is not on the curve", x, y);
+    pub fn new(x: Option<T>, y: Option<T>, a: T, b: T) -> Result<Point<T>, Error<T>> {
+        match (x, y) {
+            (None, None) => Ok(Point::Infinite { a, b }),
+            (Some(x), Some(y)) => {
+                let point = Point::Point { a, b, x, y };
+                if !point.is_valid(false) {
+                    return Err(Error::NotOnCurve(x, y));
+                }
+                Ok(point)
             }
+            _ => return Err(Error::InvalidPoint),
         }
     }
 
-    fn __pass_if_valid(self) -> Point<T> {
-        self.__ensure_valid();
-        self
+    // Checks if a non-infinite point is on the curve, thus valid
+    //
+    // When panic is set to true it will raise a panic if the point is
+    // invalid.
+    fn is_valid(&self, panic: bool) -> bool {
+        match self {
+            Self::Point { a, b, x, y } => {
+                if y.pow(2_u8) != x.pow(3_u8) + *a * *x + *b {
+                    if panic {
+                        panic!("({:?}, {:?}) is not on the curve", x, y)
+                    }
+                    return false;
+                }
+            }
+            _ => {}
+        }
+        true
+    }
+
+    // Should be called first by any operation.
+    // This is to ensure that no operations are done with invalid points.
+    //
+    // Point should never be invalid if made via the new constructor, but
+    // self constructed points could theoretically be invalid.
+    fn __ensure_valid(&self) {
+        if !self.is_valid(true) {}
     }
 }
 
@@ -74,50 +116,88 @@ impl<T: Number> std::ops::Add for Point<T> {
     fn add(self, rhs: Self) -> Self::Output {
         self.__ensure_valid();
 
-        if self.a != rhs.a || self.b != rhs.b {
-            panic!("{:?}, {:?} aren't on the same curve", self, rhs)
+        match (self, rhs) {
+            (Self::Infinite { a, b }, Self::Infinite { a: _, b: _ }) => Self::Infinite { a, b },
+            (
+                Self::Infinite { a: _, b: _ },
+                Self::Point {
+                    a: _,
+                    b: _,
+                    x: _,
+                    y: _,
+                },
+            ) => rhs,
+            (
+                Self::Point {
+                    a: _,
+                    b: _,
+                    x: _,
+                    y: _,
+                },
+                Self::Infinite { a: _, b: _ },
+            ) => self,
+            (
+                Self::Point { a, b, x, y },
+                Self::Point {
+                    a: _,
+                    b: _,
+                    x: rhs_x,
+                    y: rhs_y,
+                },
+            ) if x == rhs_x && y != rhs_y => Self::Infinite { a, b },
+            (
+                Self::Point { a, b, x, y },
+                Self::Point {
+                    a: _,
+                    b: _,
+                    x: rhs_x,
+                    y: rhs_y,
+                },
+            ) if x == rhs_x && (y == T::zero() || rhs_y == T::zero()) => Self::Infinite { a, b },
+            (
+                Self::Point { a, b, x, y },
+                Self::Point {
+                    a: _,
+                    b: _,
+                    x: rhs_x,
+                    y: rhs_y,
+                },
+            ) if x == rhs_x && y == rhs_y => {
+                let one = T::one();
+                let two = one + one;
+                let three = two + one;
+
+                let s = (three * x.pow(2_u8) + a) / (two * y);
+                let new_x = s.pow(2_u8) * x - rhs_x;
+                let new_y = s * (x - new_x) - y;
+                Point::Point {
+                    a,
+                    b,
+                    x: new_x,
+                    y: new_y,
+                }
+            }
+            (
+                Self::Point { a, b, x, y },
+                Self::Point {
+                    a: _,
+                    b: _,
+                    x: rhs_x,
+                    y: rhs_y,
+                },
+            ) if x != rhs_x => {
+                let s = (rhs_y - y) / (rhs_x - x);
+                let new_x = s.pow(2) - x - rhs_x;
+                let new_y = s * (x - new_x) - y;
+                Point::Point {
+                    a,
+                    b,
+                    x: new_x,
+                    y: new_y,
+                }
+            }
+            _ => panic!("Should never reach this point"),
         }
-
-        if self.x.is_none() {
-            return rhs;
-        }
-
-        if rhs.x.is_none() {
-            return self;
-        }
-
-        let self_x = self.x.unwrap();
-        let self_y = self.y.unwrap();
-        let rhs_x = rhs.x.unwrap();
-        let rhs_y = rhs.y.unwrap();
-
-        if self_x == rhs_x && self_y != rhs_y {
-            return Point::new(None, None, self.a, self.b);
-        }
-
-        if self_x != rhs_x {
-            let s = (rhs_y - self_y) / (rhs_x - self_x);
-            let x = s.pow(2) - self_x - rhs_x;
-            let y = s * (self_x - x) - self_y;
-            return Point::new(Some(x), Some(y), self.a, self.b);
-        }
-
-        if self == rhs && self_y == T::zero() * self_x {
-            return Point::new(None, None, self.a, self.b);
-        }
-
-        if self == rhs {
-            let one = T::one();
-            let two = one + one;
-            let three = two + one;
-
-            let s = (three * self_x.pow(2_u8) + self.a) / (two * self_y);
-            let x = s.pow(2_u8) * self_x;
-            let y = s * (self_x - x) - self_y;
-            return Point::new(Some(x), Some(y), self.a, self.b);
-        }
-
-        panic!("Should never reach this point")
     }
 }
 
@@ -127,21 +207,23 @@ mod tests {
 
     #[test]
     fn can_create_point_on_curve() {
-        Point::new(Some(-1), Some(-1), 5, 7);
+        Point::new(Some(-1), Some(-1), 5, 7).unwrap();
     }
 
     #[test]
     fn eq_and_neq_works() {
-        let a = Point::new(Some(-1), Some(-1), 5, 7);
-        let b = Point::new(Some(-1), Some(-1), 5, 7);
-        let c = Point::new(None, None, 0, 0);
+        let a = Point::new(Some(-1), Some(-1), 5, 7).unwrap();
+        let b = Point::new(Some(-1), Some(-1), 5, 7).unwrap();
+        let c = Point::new(None, None, 0, 0).unwrap();
         assert_eq!(a, b);
         assert_ne!(a, c);
     }
 
     #[test]
-    #[should_panic(expected = "(-1, -2) is not on the curve")]
     fn can_not_create_point_not_on_curve() {
-        Point::new(Some(-1), Some(-2), 5, 7);
+        assert_eq!(
+            Point::new(Some(-1), Some(-2), 5, 7),
+            Err(Error::NotOnCurve(-1, -2))
+        );
     }
 }
